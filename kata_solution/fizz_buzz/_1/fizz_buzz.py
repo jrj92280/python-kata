5e4815c215be9bb0e0713ebac70ca3b42e9bb715
jrj92280 <jrj92280@gmail.com>
2018-05-16 11:25:10 -0400

    Archive FizzBuzz practice
    updated FizzBuzz template

---
+++ kata_solution/fizz_buzz/_1/fizz_buzz.py
@@ -0,0 +1,14 @@
+input = 100
+
+
+def fizz_buzz(upperbound):
+    for number in range(1, upperbound + 1):
+        if number % 3 == 0:
+            print('fizz')
+        elif number == 100:
+            print('2222')
+        else:
+            print(number)
+
+
+fizz_buzz(input)
---
+++ katas/fizz_buzz/fizz_buzz_test.py
@@ -0,0 +1,17 @@
+"""
+Z - Zero
+O - One
+M - Many
+B - Boundary Behaviors
+I - Interface Definition
+E - Exercise Exceptional
+S - Simple Scenarios, Simple Solutions
+"""
+
+"""
+TDD : Test Driven Development
+
+1. You are not allowed to write any production code unless it is to make a failing unit test pass.
+2. You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+3. You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+"""
