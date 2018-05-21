df1f0b9b91a6c687e7abef2630e8af8e7d5ee778
jrj92280 <jrj922@gmail.com>
2018-05-21 13:28:04 -0400

    fix typo in kata

---
+++ katas/number_guessing_game/guessing_game.py
@@ -0,0 +1,11 @@
+print('Guess the number')
+
+print(' : Rules')
+
+print(" : Guess a number between 1 through 100")
+
+print(' : If you guess it within 4 tries you win')
+
+print ('------------------------------------------------ ')
+
+
