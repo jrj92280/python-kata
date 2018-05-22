9dc92ff313ebd47278fa37643e464ad356bd6886
jrj92280 <jrj922@gmail.com>
2018-05-22 14:41:37 -0400

    fix typo in kata

--- katas/number_guessing_game/guessing_game.py
+++ katas/number_guessing_game/guessing_game.py
@@ -1,3 +1,5 @@
+import random
+
 print('Guess the number')
 
 print(' : Rules')
@@ -8,4 +10,17 @@ print(' : If you guess it within 4 tries you win')
 
 print ('------------------------------------------------ ')
 
+numbers = random.randint(0, 100)
+guess = -1
+
+while guess != numbers:
+Guess_text = input('Guess a number 0 to 100:')
+guess = int(guess_text)
+
+    if guess < numbers:
+    print('to low')
+    elif guess > numbers:
+    print("to high")
+    else:
+    print('winner winner chiken dinner!!!!')
 
