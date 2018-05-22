4d7416d5a9e42d5910e50940e878062ea001d57f
jrj92280 <jrj922@gmail.com>
2018-05-22 15:53:31 -0400

    Add number guessing game

--- katas/number_guessing_game/guessing_game.py
+++ katas/number_guessing_game/guessing_game.py
@@ -9,18 +9,28 @@ print(" : Guess a number between 1 through 100")
 print(' : If you guess it within 4 tries you win')
 
 print ('------------------------------------------------ ')
+max_guesses = 5
+
+guesses = 1
 
 numbers = random.randint(0, 100)
 guess = -1
-
 while guess != numbers:
-Guess_text = input('Guess a number 0 to 100:')
-guess = int(guess_text)
+    if guesses > max_guesses:
+        print('you lose try again...\nnumber to guess been reset')
+        numbers = random.randint(0, 100)
+        guesses = 1
+        continue
+
+    guess_text = input('Guess a number 0 to 100:')
+    guess = int(guess_text)
 
     if guess < numbers:
-    print('to low')
+        print('to low')
     elif guess > numbers:
-    print("to high")
+        print("to high")
     else:
-    print('winner winner chiken dinner!!!!')
+        print('winner winner chiken dinner!!!!')
+
+    guesses = guesses + 1
 
