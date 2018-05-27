9382654f42c25b3ce144ca9b23290af6e36b90bb
jrj92280 <jrj922@gmail.com>
2018-05-26 23:54:14 -0400

    Practice odd number

--- head_first_python/chapter_1/odd.py
+++ head_first_python/chapter_1/odd.py
@@ -6,5 +6,7 @@ right_this_minute = datetime.today().minute
 print(right_this_minute)
 if right_this_minute in odds:
     print("odd ball")
+elif right_this_minute==6:
+    print("happy soul")
 else:
     print("even steven")
