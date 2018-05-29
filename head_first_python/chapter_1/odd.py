fee617d4e232820b05a4613d7db91165fa72b583
jrj92280 <jrj922@gmail.com>
2018-05-29 11:41:56 -0400

    Practice odd number

--- head_first_python/chapter_1/odd.py
+++ head_first_python/chapter_1/odd.py
@@ -1,12 +1,23 @@
 from datetime import datetime
 
-odds = [number for number in range(1, 60, 2)]
+import random
+import time
+
+
+
+odds = [number for number in range(1, 60, 2 )]
 print(odds)
 right_this_minute = datetime.today().minute
 print(right_this_minute)
-if right_this_minute in odds:
-    print("odd ball")
-elif right_this_minute==6:
-    print("happy soul")
-else:
-    print("even steven")
+
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("odd ball")
+    elif right_this_minute==6:
+        print("happy soul")
+    else:
+        print("even steven")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
