470a49743e430538c39bc02f656989ecb95895d0
jrj92280 <jrj92280@gmail.com>
2018-06-04 18:01:11 -0400

    Practice Roman Calculator kata

--- katas/roman_calculator/roman_calculator.py
+++ katas/roman_calculator/roman_calculator.py
@@ -1,5 +1,17 @@
 def roman_calculator(roman_numeral):
-    return roman_numeral
+    total = 0
+
+    for numeral in roman_numeral:
+        # todo: find numeral of character
+        numeral_value = 1
+        if numeral == 'V':
+            numeral_value = 5
+        elif numeral == 'X':
+            numeral_value = 10
+
+        total = total + numeral_value
+
+    return total
 
 
 if __name__ == '__main__':
--- katas/roman_calculator/roman_calculator_test.py
+++ katas/roman_calculator/roman_calculator_test.py
@@ -15,3 +15,41 @@ TDD : Test Driven Development
 2. You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
 3. You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
 """
+
+"""
+**There are some rules to a Roman number:**
+- Numerals can be concatenated to form a larger numeral (“XX” + “II” = “XXII”)
+- If a lesser numeral is put before a bigger it means subtraction of the lesser from the bigger (“IV” means four, “CM” means ninehundred)
+- If the numeral is I, X or C you can’t have more than three (“II” + “II” = “IV”)
+- If the numeral is V, L or D you can’t have more than one (“D” + “D” = “M”)
+"""
+
+from katas.roman_calculator.roman_calculator import roman_calculator
+
+
+def test_zero():
+    assert 0 == roman_calculator("")
+
+
+def test_one():
+    assert 1 == roman_calculator("I")
+
+
+def test_two():
+    assert 2 == roman_calculator("II")
+
+
+def test_three():
+    assert 3 == roman_calculator("III")
+
+
+def test_five():
+    assert 5 == roman_calculator("V")
+
+
+def test_six():
+    assert 6 == roman_calculator("VI")
+
+
+def test_ten():
+    assert 10 == roman_calculator("X")
