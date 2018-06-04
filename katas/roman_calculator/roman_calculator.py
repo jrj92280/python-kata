b18ffda7bcc6ab129e1ceaca2b3a9330d43f26a1
jrj92280 <jrj92280@gmail.com>
2018-06-04 17:16:44 -0400

    Add roman calculator

---
+++ katas/roman_calculator/roman_calculator.py
@@ -0,0 +1,6 @@
+def roman_calculator(roman_numeral):
+    return roman_numeral
+
+
+if __name__ == '__main__':
+    print(roman_calculator('IV'))
---
+++ katas/roman_calculator/roman_calculator_test.py
@@ -0,0 +1,17 @@
+"""
+Z - Zero x
+O - One x
+M - Many x
+B - Boundary Behaviors
+I - Interface Definition x
+E - Exercise Exceptional x
+S - Simple Scenarios, Simple Solutions
+"""
+
+"""
+TDD : Test Driven Development
+
+1. You are not allowed to write any production code unless it is to make a failing unit test pass.
+2. You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+3. You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+"""
