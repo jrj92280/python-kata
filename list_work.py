fe0854b4c482dbefd8909a53da47664790eb86a5
jrj92280 <jrj922@gmail.com>
2018-06-07 23:26:30 -0400

    List practice

---
+++ list_work.py
@@ -0,0 +1,12 @@
+vowels = ['a','e','i','o','u']
+word ="milliways"
+for letter in word:
+    if letter in vowels:
+        print(letter)
+found=[]
+len(found)
+found.append("a")
+found.append('e')
+found.append('i')
+print(len(found))
+print (found[0])
