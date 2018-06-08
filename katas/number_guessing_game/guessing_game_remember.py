a9d61078a8bb10966cf33bb36ad6aabab20722d5
jrj92280 <jrj92280@gmail.com>
2018-06-08 14:27:05 -0400

    Practice Roman Calculator kata

--- katas/number_guessing_game/guessing_game_remember.py
+++ katas/number_guessing_game/guessing_game_remember.py
@@ -17,6 +17,12 @@ lower_int = 0
 
 def game_event_loop():
     global numbers, guesses, guess, top_int, lower_int
+
+    set_number_guesses_text = '\nPlayer please choose your amount of guesses: '
+    amount_of_guesses = get_user_input(set_number_guesses_text)
+
+    max_guesses = amount_of_guesses
+
     while guess != numbers:
         if guesses > max_guesses:
             # print my guesses and the actual value
@@ -31,11 +37,13 @@ def game_event_loop():
             guesses = 1
             continue
 
-        guess_text = input('\n'
-                           'Guess a number ' + str(lower_int) + ' to ' + str(top_int) + ': ')
-        tracking_list.append(guess_text)
+        game_text = '\n\nRemaining guesses: ' + str(amount_of_guesses) + \
+                    '\nGuess a number ' + str(lower_int) + ' to ' + str(top_int) + ': '
+        guess = get_user_input(game_text)
 
-        guess = int(guess_text)
+        tracking_list.append(guess)
+
+        amount_of_guesses = amount_of_guesses - 1
 
         if guess < numbers:
             print('to low')
@@ -48,6 +56,16 @@ def game_event_loop():
             print('winner winner chiken dinner!!!!')
 
         guesses = guesses + 1
+
+
+def get_user_input(text):
+    while True:
+        amount_of_guesses = input(text)
+        try:
+            amount_of_guesses = int(amount_of_guesses)
+            return amount_of_guesses
+        except:
+            print('NUMBERS ONLY')
 
 
 game_event_loop()
