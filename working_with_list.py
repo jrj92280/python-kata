ea2916d1b66c0a2b00cc7a0a18650c1426609120
jrj92280 <jrj92280@gmail.com>
2018-06-10 13:13:57 -0400

    Archive programming 101 practice

--- working_with_list.py
+++ working_with_list.py
@@ -3,7 +3,8 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 words = [
     'milliways', 'hitch_hiker', 'galaxy', 'sky',
     'milliways', 'hitch_hiker', 'galaxy', 'sky',
-    'milliways', 'hitch_hiker', 'galaxy', 'sky'
+    'milliways', 'hitch_hiker', 'galaxy', 'sky',
+    'jason', 'dog'
 ]
 
 print(words)
@@ -11,12 +12,27 @@ print(words)
 found = []
 found_words = []
 found_words_set = set()
+found_words_dict = dict()
+
+for word in words:
+    if word in found_words_dict:
+        found_words_dict[word] = found_words_dict[word] + 1
+    else:
+        found_words_dict[word] = 1
+
+print(found_words_dict)
+
+for key in found_words_dict:
+    if found_words_dict[key] == 1:
+        print(key)
+
 
 for word in words:
     # words
     if word not in found_words:
         found_words.append(word)
 
+    # set
     found_words_set.add(word)
 
     # vowels
