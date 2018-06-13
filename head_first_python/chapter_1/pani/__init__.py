d6bc2e9a2afbe77af646a5945510d940452bb3bb
jrj92280 <jrj922@gmail.com>
2018-06-13 15:04:57 -0400

    pop a list practice

--- head_first_python/chapter_1/pani/__init__.py
+++ head_first_python/chapter_1/pani/__init__.py
@@ -1,14 +1,37 @@
-phrase = "don't panic!"
-plist = list(phrase)
-print (phrase)
-print (plist)
-plist.pop(0)
-plist.pop(2)
-plist.pop(6);plist.pop(6);plist.pop(6)
-plist.insert(2,plist.pop(3))
-plist.insert(4,plist.pop(5))
-plist.remove("!")
-new_phrase = ''.join(plist)
+#phrase = "don't panic!"
+#plist = list(phrase)
+#print (phrase)
+#print (plist)
 
-print (plist)
-print (new_phrase)
+#plist.pop(0)
+#plist.pop(2)
+#plist.pop(6);plist.pop(6);plist.pop(6)
+#plist.insert(2,plist.pop(3))
+#plist.insert(4,plist.pop(5))
+#plist.remove("!")
+#new_phrase = ''.join(plist)
+
+#print (plist)
+#print (new_phrase)
+##for (i) in range(4):
+   ## plist.pop()
+
+##plist.pop(0)
+##plist.remove("'")
+##plist.extend([plist.pop(),plist.pop()])
+##plist.insert(2,plist.pop(3))
+
+##new_phrase = ''.join(plist)
+##print (plist)
+##print (new_phrase
+
+saying = "don't panic!"
+letters = list(saying)
+letters
+['d','o','n',"'",'t','p','a','n','i','c','!']
+
+
+
+first = letters[0]
+last = letters[-1]
+
