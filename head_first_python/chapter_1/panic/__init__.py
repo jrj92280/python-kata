f8ed2fb5f4beec0b28b13eeb56f583fd4b9ec4af
jrj92280 <jrj92280@gmail.com>
2018-06-14 23:02:44 -0400

    add list slicing practice

--- head_first_python/chapter_1/panic/__init__.py
+++ head_first_python/chapter_1/panic/__init__.py
@@ -28,26 +28,28 @@
 saying = "don't panic!"
 letters = list(saying)
 letters =['d','o','n',"'",'t','p','a','n','i','c','!']
-print (saying)
-print (letters)
+###print (saying)
+##rint (letters)
 #backwards_letters =letters[::-1]
 #print (backwards_letters)
-tip = letters[4:9]
+##tip = letters[4:9]
 # tpani
 
-tip.pop(2)
+##tip.pop(2)
 # tpni
 
-tip.pop(2)
+##tip.pop(2)
 # tpi
 
 
 ## TPI
 ## TP  x=I
 ## insert(2,x)
-pop_value = tip.pop(1)
-tip.insert(2, pop_value)
-print (tip)
-first = letters[0]
-last = letters[-1]
-
+##pop_value = tip.pop(1)
+##tip.insert(2, pop_value)
+##print (tip)
+##first = letters[0]
+##last = letters[-1]
+##print(first,last)
+three = ''.join(letters[0:10:3])
+print(three)
