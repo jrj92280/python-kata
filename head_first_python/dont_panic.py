0731bd1903502a55ea16bc44a4271305e96c66ca
jrj92280 <jrj92280@gmail.com>
2018-06-16 23:48:40 -0400

    practice looping

--- head_first_python/dont_panic.py
+++ head_first_python/dont_panic.py
@@ -5,7 +5,6 @@ new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
 
-new_phrase = ''.join(plist[1:3])
-new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
+new_phrase = ''.join(plist[1:3] + [plist[5], plist[4], plist[7], plist[6]])
 print(plist)
 print(new_phrase)
