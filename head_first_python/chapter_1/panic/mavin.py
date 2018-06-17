68f721f4f2c3f772b32f4a694245f28f1d1a1c7e
jrj92280 <jrj92280@gmail.com>
2018-06-16 23:35:50 -0400

    practice looping

--- head_first_python/chapter_1/panic/mavin.py
+++ head_first_python/chapter_1/panic/mavin.py
@@ -1,4 +1,16 @@
-paranoid_android = "Marvin"
+# aranoid_android = "Marvin"
+# letters = list(paranoid_android)
+# for char in letters:
+#    print('\t', char)
+
+
+paranoid_android = "marvin, the Paranoid Android"
 letters = list(paranoid_android)
-for char in letters:
+for char in letters[:6]:
     print('\t', char)
+print()
+for char in letters[-7:]:
+    print('\t' * 2, char)
+print()
+for char in letters[12:20]:
+    print('\t' * 3, char)
