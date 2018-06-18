7723f7ad11bcc73bbc664e34a9a8416c91d77511
jrj92280 <jrj92280@gmail.com>
2018-06-18 19:03:46 -0400

    add players to chess game

--- chess_game_practice/play_game.py
+++ chess_game_practice/play_game.py
@@ -3,14 +3,18 @@ from chess_game_practice.chess_game import ChessGame
 
 
 def game_event_loop(chess_game):
+    current_move_number = 1
+
     while True:
         print(chess_game)
 
+        print('Player: ' + str(current_move_number % 2))
         game_text = '\n\nMake A Move (x,y,x2,y2): '
         move = get_user_input(game_text)
         current_position = [int(move[0]), int(move[1])]
         target_position = [int(move[2]), int(move[3])]
         chess_game.move_piece(current_position, target_position)
+        current_move_number = current_move_number + 1
 
 
 def get_user_input(text):
@@ -34,9 +38,17 @@ def get_user_input(text):
 if __name__ == "__main__":
     game_board = make_board()
 
-    print('play chess')
+    print('Chess')
     print(' : Rules')
-    print(' : Enter a move')
+    print('   : input - piece''s position x,y, second x,y = destination')
+    print("   : x = row number 1 though 8")
+    print("   : y = column number 1 though 8")
+    player1_name = input(' : Enter player one name')
+    player2_name = input(' : Enter player two name')
+    moves = {
+        player1_name: list(),
+        player2_name: list()
+    }
     print('------------------------------------------------')
 
     chess_game = ChessGame(game_board)
