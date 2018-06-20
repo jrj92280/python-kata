154f315e04684b11c6fdf899e7ccf83d112a98c9
jrj92280 <jrj922@gmail.com>
2018-06-20 13:49:28 -0400

    extracting vowels

--- chapter_3/dict.py
+++ chapter_3/dict.py
@@ -3,16 +3,16 @@ word = input('Provide a word to search for vowels: ')
 
 found = {}
 
-found['a'] = 0
-found['e'] = 0
-found['i'] = 0
-found['o'] = 0
-found['u'] = 0
+#found['a'] = 0
+#found['e'] = 0
+#found['i'] = 0
+#found['o'] = 0
+#found['u'] = 0
 
 for letter in word:
     if letter in vowels:
         found[letter] += 1
 
-for k, v in sorted (found.items()):
-    print(k, 'was found', v, 'time(s).')
+for g, c in sorted(found.items()):
+    print(g, 'was found', c, 'time(s).')
 
