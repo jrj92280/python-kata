98a51fbe6357ff14e8fe6d7efcc429ada9cdc7c9
jrj92280 <jrj922@gmail.com>
2018-06-25 00:22:10 -0400

    Created cards for texas holdem

---
+++ poker/card_setup.py
@@ -0,0 +1,12 @@
+import random
+cards = ['A, spades','A, clubs', 'A, hearts', 'A, dimonds', 'K, spades','K, clubs', 'K, hearts', 'K, dimonds',
+        'Q, spades','Q, clubs', 'Q, hearts', 'Q, dimonds','J, spades','J, clubs', 'J, hearts', 'J, dimonds',
+        '10, spades','10, clubs', '10, hearts', '10, dimonds','9, spades','9, clubs', '9, hearts', '9, dimonds',
+        '8, spades','8, clubs', '8, hearts', '8, dimonds','7, spades','7, clubs', '7, hearts', '7, dimonds',
+        '6, spades','6, clubs', '6, hearts', '6, dimonds','5, spades','5, clubs', '5, hearts', '5, dimonds',
+        '4, spades','4, clubs', '4, hearts', '4, dimonds','3, spades','3, clubs', '3, hearts', '3, dimonds',
+        '2, spades','2, clubs', '2, hearts', '2, dimonds']
+game = cards = random.randit + cards
+game = cards[3]
+
+print (game)
