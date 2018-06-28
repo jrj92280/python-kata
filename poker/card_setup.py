2eaf35d8c38c449f602061fa23f08fece2bffad1
jrj92280 <jrj92280@gmail.com>
2018-06-27 23:39:42 -0400

    Update chess game

--- poker/card_setup.py
+++ poker/card_setup.py
@@ -8,25 +8,28 @@ player_names = []
 for player in range(player_count):
     player_name = input('name:')
     player_names.append(player_name)
+    player_hand = player_hand
 
-cards = ['A, spades', 'A, clubs', 'A, hearts', 'A, dimonds',
-         'K, spades', 'K, clubs', 'K, hearts', 'K, dimonds',
-         'Q, spades', 'Q, clubs', 'Q, hearts', 'Q, dimonds',
-         'J, spades', 'J, clubs', 'J, hearts', 'J, dimonds',
-         '10, spades', '10, clubs', '10, hearts', '10, dimonds',
-         '9, spades', '9, clubs', '9, hearts', '9, dimonds',
-         '8, spades', '8, clubs', '8, hearts', '8, dimonds',
-         '7, spades', '7, clubs', '7, hearts', '7, dimonds',
-         '6, spades', '6, clubs', '6, hearts', '6, dimonds',
-         '5, spades', '5, clubs', '5, hearts', '5, dimonds',
-         '4, spades', '4, clubs', '4, hearts', '4, dimonds',
-         '3, spades', '3, clubs', '3, hearts', '3, dimonds',
-        '2, spades','2, clubs', '2, hearts', '2, dimonds']
+cards = ['A, spades', 'A, clubs', 'A, hearts', 'A, diamonds',
+         'K, spades', 'K, clubs', 'K, hearts', 'K, diamonds',
+         'Q, spades', 'Q, clubs', 'Q, hearts', 'Q, diamonds',
+         'J, spades', 'J, clubs', 'J, hearts', 'J, diamonds',
+         '10, spades', '10, clubs', '10, hearts', '10, diamonds',
+         '9, spades', '9, clubs', '9, hearts', '9, diamonds',
+         '8, spades', '8, clubs', '8, hearts', '8, diamonds',
+         '7, spades', '7, clubs', '7, hearts', '7, diamonds',
+         '6, spades', '6, clubs', '6, hearts', '6, diamonds',
+         '5, spades', '5, clubs', '5, hearts', '5, diamonds',
+         '4, spades', '4, clubs', '4, hearts', '4, diamonds',
+         '3, spades', '3, clubs', '3, hearts', '3, diamonds',
+         '2, spades', '2, clubs', '2, hearts', '2, diamonds']
 
 deck = set(itertools.product(cards))
 drawn_cards = random.sample(deck, (5 + 2 * player_count))
 
 players = []
-player
+
 random.shuffle(cards)
+player_hand = player_hand
 print (cards)
+player_hand = player_hand
--- poker/texas_holdem/__init__.py
+++ poker/texas_holdem/__init__.py
@@ -1,3 +1,12 @@
-class Card:
-    def __init__(self, cards= 0):
+class card:
+    def __init__(self, cards):
         self.cards = cards
+        self.card = self.cards
+
+    def __repr__(self):
+        return self.cards
+
+
+class poker_hand():
+    def __init__(self, card_list):
+        self.card_list = card_list
