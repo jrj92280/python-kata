1b47b6e1308117c735f5ba99e6298111969cf5ba
jrj92280 <jrj922@gmail.com>
2018-07-02 15:03:40 -0400

    Dealing cards to players

---
+++ chapter_3/__init__.py
@@ -0,0 +1,23 @@
+fruits = {}
+fruits['apples'] = 10
+fruits
+'apples : 10'
+'apples'in fruits
+if 'pears' not in fruits:
+    fruits['pears'] = 0
+fruits['pears'] += 1
+fruits
+
+
+if 'bananas' in fruits:
+    fruits['bananas'] += 1
+else:
+    fruits['bananas'] = 1
+
+fruits
+{'bananas': 2,
+ 'pears':2,
+ 'apples': 10}
+
+
+print (fruits)
