e42f6fcb1ace6ca5f2782097ff4196ed5a5cb99f
jrj92280 <jrj922@gmail.com>
2018-07-04 23:55:45 -0400

    Wrote setdefault method

--- chapter_3/__init__.py
+++ chapter_3/__init__.py
@@ -1,24 +1,33 @@
 
-fruits {}
-fruits['apples'] = 10
-fruits
-'apples : 10'
-'apples'in fruits
-if 'pears' not in fruits:
-    fruits['pears'] = 0
-fruits['pears'] += 1
-fruits
+#fruits  = {}
+#fruits['apples'] = 10
+#fruits
+#'apples : 10'
+#'apples'in fruits
+#if 'pears' not in fruits:
+#    fruits['pears'] = 0
+#fruits['pears'] += 1
+#fruits
 
 
-if 'bananas' in fruits:
-    fruits['bananas'] += 1
-else:
-    fruits['bananas'] = 1
+#if 'bananas' in fruits:
+ #   fruits['bananas'] += 1
+#else:
+#    fruits['bananas'] = 1
 
-fruits
-{'bananas': 2,
- 'pears':2,
- 'apples': 10}
+#fruits
+#{'bananas': 2,
+# 'pears':2,
+##': 10}
 
+vowels = ['a','e', 'i', 'o', 'u']
+word = input('Provide a word to search for vowels: ')
 
-print (fruits)
+found = {}
+ for letter in word :
+     if letter in word:
+         if letter in vowels:
+             found[letter] +=1
+
+for k,v in sorted(found.items()):
+    print(k, 'was found',v, 'time(s). ')
