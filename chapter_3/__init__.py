54952fd93790046dbb1dd1326f09ab0f6163a3b2
jrj92280 <jrj922@gmail.com>
2018-07-05 23:04:48 -0400

    Wrote setdefault method

--- chapter_3/__init__.py
+++ chapter_3/__init__.py
@@ -20,14 +20,16 @@
 # 'pears':2,
 ##': 10}
 
-vowels = ['a','e', 'i', 'o', 'u']
+vowels = ['a', 'e', 'i', 'o', 'u']
 word = input('Provide a word to search for vowels: ')
 
 found = {}
- for letter in word :
-     if letter in word:
-         if letter in vowels:
-             found[letter] +=1
 
-for k,v in sorted(found.items()):
-    print(k, 'was found',v, 'time(s). ')
+for letter in word:
+    if letter in vowels:
+        found.setdefault(letter, 0)
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s). ')
+
