6c23e9172e6088aa743b18f661f67314ea8e2a11
jrj92280 <jrj92280@gmail.com>
2018-07-07 23:46:46 -0400

    Created card number values

--- poker/texas_holdem/evaluate_hand.py
+++ poker/texas_holdem/evaluate_hand.py
@@ -1,6 +1,30 @@
+from collections import defaultdict
+
+
 def evaluate_hand(hand):
-    return sorted(hand, key=sort_cards, reverse=True)[0:5]
+    same_cards = defaultdict(int)
+
+    for card in hand:
+        card_value = get_card_value(card)
+        same_cards[card_value] += 1
+
+    pairs = []
+    for card_value, card_count in same_cards.items():
+        if card_count == 2:
+            pair = []
+            for card in hand:
+                if get_card_value(card) == card_value:
+                    pair.append(card)
+            pairs.append(pair)
+
+    high_cards = sorted(hand, key=sort_cards, reverse=True)[0:5]
+    return [high_cards, pairs]
 
 
 def sort_cards(card):
+    return get_card_value(card)
+
+
+def get_card_value(card):
     return int(card.split(',')[0])
+
--- poker/texas_holdem/evaluate_hand_test.py
+++ poker/texas_holdem/evaluate_hand_test.py
@@ -6,26 +6,41 @@ def test_hand_has_7_cards():
 
     current_hand = evaluate_hand(hand)
 
-    assert 5 == len(current_hand)
+    assert 5 == len(current_hand[0])
 
 
 def test_high_cards():
-    exected_hand = ['14,A,clubs', '14,A,diamonds', '14,A,hearts', '14,A,spades', '4,4,spades']
+    exected_hand = [['14,A,clubs', '14,A,diamonds', '14,A,hearts', '14,A,spades', '4,4,spades'], []]
 
     hand = ['14,A,spades', '14,A,clubs', '14,A,hearts', '14,A,diamonds', '2,2,spades', '3,3,hearts', '4,4,spades']
     current_hand = evaluate_hand(hand)
 
-    _exected_hand = set(exected_hand)
-    _current_hand = set(current_hand)
+    _exected_hand = set(exected_hand[0])
+    _current_hand = set(current_hand[0])
 
     assert _exected_hand == _current_hand
 
 
 def test_high_cards_two():
-    exected_hand = ['13,K,hearts', '12,Q,clubs', '11,J,spades', '10,10,diamonds', '6,6,spades']
+    exected_hand = [['13,K,hearts', '12,Q,clubs', '11,J,spades', '10,10,diamonds', '6,6,spades'], []]
 
     hand = ['11,J,spades', '12,Q,clubs', '13,K,hearts', '10,10,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
 
     current_hand = evaluate_hand(hand)
 
     assert exected_hand == current_hand
+
+
+def test_one_pair():
+    exected_hand = (
+    ['13,K,hearts', '13,K,clubs', '11,J,spades', '10,10,diamonds', '6,6,spades'], [['13,K,hearts', '13,K,clubs']])
+
+    hand = ['11,J,spades', '13,K,hearts', '13,K,clubs', '10,10,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
+
+    current_hand = evaluate_hand(hand)
+
+    _exected_hand = set(exected_hand[0])
+    _current_hand = set(current_hand[0])
+
+    assert _exected_hand == _current_hand
+    assert exected_hand[1] == current_hand[1]
