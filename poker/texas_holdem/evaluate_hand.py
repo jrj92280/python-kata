f252b7db8a5534ee922f396950ac7efb8a6b3ffe
jrj92280 <jrj92280@gmail.com>
2018-07-08 20:18:55 -0400

    Created card number values

--- poker/texas_holdem/evaluate_hand.py
+++ poker/texas_holdem/evaluate_hand.py
@@ -2,23 +2,45 @@ from collections import defaultdict
 
 
 def evaluate_hand(hand):
-    same_cards = defaultdict(int)
+    card_groups = create_groups(hand)
 
+    high_cards = sorted(hand, key=sort_cards, reverse=True)[0:5]
+    pairs = group_cards(2, card_groups, hand)
+    three_of_kinds = group_cards(3, card_groups, hand)
+    four_of_kind = group_cards(4, card_groups, hand)
+    full_house = []
+
+    if len(pairs) and len(three_of_kinds):
+        full_house.extend(three_of_kinds[0])
+        # ps
+        pairs_index = 0
+        if len(pairs) == 2 and get_card_value(pairs[0][0]) < get_card_value(pairs[1][0]):
+            pairs_index = 1
+        full_house.extend(pairs[pairs_index])
+
+    return [high_cards, pairs, three_of_kinds, four_of_kind, full_house]
+
+
+def create_groups(hand):
+    same_cards = defaultdict(int)
     for card in hand:
         card_value = get_card_value(card)
         same_cards[card_value] += 1
+    return same_cards
 
-    pairs = []
-    for card_value, card_count in same_cards.items():
-        if card_count == 2:
-            pair = []
+
+def group_cards(group_count, card_groups, hand):
+    groups = []
+
+    for card_value, card_count in card_groups.items():
+        if card_count == group_count:
+            group = []
             for card in hand:
                 if get_card_value(card) == card_value:
-                    pair.append(card)
-            pairs.append(pair)
+                    group.append(card)
+            groups.append(group)
 
-    high_cards = sorted(hand, key=sort_cards, reverse=True)[0:5]
-    return [high_cards, pairs]
+    return groups
 
 
 def sort_cards(card):
@@ -27,4 +49,3 @@ def sort_cards(card):
 
 def get_card_value(card):
     return int(card.split(',')[0])
-
--- poker/texas_holdem/evaluate_hand_test.py
+++ poker/texas_holdem/evaluate_hand_test.py
@@ -10,19 +10,19 @@ def test_hand_has_7_cards():
 
 
 def test_high_cards():
-    exected_hand = [['14,A,clubs', '14,A,diamonds', '14,A,hearts', '14,A,spades', '4,4,spades'], []]
+    exected_hand = ['14,A,clubs', '14,A,diamonds', '14,A,hearts', '14,A,spades', '4,4,spades']
 
     hand = ['14,A,spades', '14,A,clubs', '14,A,hearts', '14,A,diamonds', '2,2,spades', '3,3,hearts', '4,4,spades']
     current_hand = evaluate_hand(hand)
 
-    _exected_hand = set(exected_hand[0])
+    _exected_hand = set(exected_hand)
     _current_hand = set(current_hand[0])
 
     assert _exected_hand == _current_hand
 
 
 def test_high_cards_two():
-    exected_hand = [['13,K,hearts', '12,Q,clubs', '11,J,spades', '10,10,diamonds', '6,6,spades'], []]
+    exected_hand = [['13,K,hearts', '12,Q,clubs', '11,J,spades', '10,10,diamonds', '6,6,spades'], [], [], [], []]
 
     hand = ['11,J,spades', '12,Q,clubs', '13,K,hearts', '10,10,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
 
@@ -44,3 +44,43 @@ def test_one_pair():
 
     assert _exected_hand == _current_hand
     assert exected_hand[1] == current_hand[1]
+
+
+def test_one_three_of_kind():
+    exected_hand = [['13,K,hearts', '13,K,clubs', '13,K,spades']]
+
+    hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '10,10,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
+
+    current_hand = evaluate_hand(hand)
+
+    assert exected_hand == current_hand[2]
+
+
+def test_four_of_kind():
+    exected_hand = [['13,K,hearts', '13,K,clubs', '13,K,spades', '13,K,diamonds']]
+
+    hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '13,K,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
+
+    current_hand = evaluate_hand(hand)
+
+    assert exected_hand == current_hand[3]
+
+
+def test_full_house():
+    exected_hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '6,6,diamonds', '6,6,spades']
+
+    hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '6,6,diamonds', '6,6,spades', '3,3,hearts', '4,4,spades']
+
+    current_hand = evaluate_hand(hand)
+
+    assert exected_hand == current_hand[4]
+
+
+def test_full_house_with_two_pairs():
+    exected_hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '6,6,diamonds', '6,6,spades']
+
+    hand = ['13,K,hearts', '13,K,clubs', '13,K,spades', '4,4,hearts', '4,4,spades', '6,6,diamonds', '6,6,spades']
+
+    current_hand = evaluate_hand(hand)
+
+    assert exected_hand == current_hand[4]
