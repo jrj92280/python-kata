de6897bee6fbe622634b53698d84d95cf00ec742
jrj92280 <jrj92280@gmail.com>
2018-07-10 22:59:19 -0400

    Working with a set

--- chapter_3/sets_tuple.py
+++ chapter_3/sets_tuple.py
@@ -1,2 +1,14 @@
 vowels = {'a', 'e', 'i', 'o', 'u', 'a', 'a', 'i', 'i'}
 print (vowels)
+
+vowels2 = set('aeeiouu')
+print(vowels2)
+word = 'hello'
+u = vowels.union(set(word))
+print(u)
+u_list = sorted(list(u))
+print(u_list)
+d = vowels.difference(set(word))
+print (d)
+i = vowels.intersection(set(word))
+print (i)
