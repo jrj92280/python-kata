5d8a407ae7368a17ea43406b872c05820deda132
jrj92280 <jrj92280@gmail.com>
2018-07-17 17:31:19 -0400

    Calculator creation

---
+++ home_work/home_work_3.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+def calculator(x,y,op):
+
+    return 5,7,'+'  == 12
+x=(int)
+y=(int)
+op = ('+','-','*','/')
+
+
+
+calculator(5,7,'+')
