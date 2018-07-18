891df2b5830d13f0ddb7d65ea28aad750e730748
jrj92280 <jrj92280@gmail.com>
2018-07-17 22:57:54 -0400

    find largest number function

--- home_work/_1.py
+++ home_work/_1.py
@@ -1,6 +1,6 @@
 
 def add(x, y):
-     return print(x+y)
+    return print(x+y)
 add(4,5)
 add(-4,3)
 
--- home_work/home_work_3.py
+++ home_work/home_work_3.py
@@ -1,13 +1,1 @@
-from sys import argv
 
-script, first, second, third = argv
-def calculator(x,y,op):
-
-    return 5,7,'+'  == 12
-x=(int)
-y=(int)
-op = ('+','-','*','/')
-
-
-
-calculator(5,7,'+')
