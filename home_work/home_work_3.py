b91f1adf3aedd6b3dda677645aa1174592e00c72
jrj92280 <jrj92280@gmail.com>
2018-07-17 23:46:03 -0400

    Calculator

--- home_work/home_work_3.py
+++ home_work/home_work_3.py
@@ -1,1 +1,21 @@
 
+def opperation(x, y, op):
+
+        return opperation
+add = (3, 5, '+')
+assert 8 == opperation(add)
+
+sub = (3, 5, '-')
+assert -2 == opperation(sub)
+
+multi = (6, 2, '*')
+assert 12 == opperation(multi)
+
+div = (6, 2, '/')
+assert 3 == opperation(add)
+
+
+
+
+
+
