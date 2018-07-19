bc34c15c0c563f8abe2e817ad381deb8f05d8ac1
jrj92280 <jrj92280@gmail.com>
2018-07-18 22:18:41 -0400

    Update card face value

--- poker/texas_holdem/display_card.py
+++ poker/texas_holdem/display_card.py
@@ -1,11 +1,30 @@
 def display_card(card):
     suit = card[0]
+    card_value = card[1:]
 
     if suit == 'H':
         suit = '♥'
 
-    card_value = card[1:]
+    if suit == 'C':
+        suit = '♣'
 
-    # if 11 card value = J
+    if suit == 'S':
+        suit = '♠'
+
+    if suit == 'D':
+        suit = '♦'
+
+    if card_value == '11':
+        card_value = 'J'
+
+    if card_value == '12':
+        card_value = 'Q'
+
+    if card_value == '13':
+        card_value = 'K'
+
+    if card_value == '14':
+        card_value = 'A'
+
 
     return card_value + suit
--- poker/texas_holdem/display_card_test.py
+++ poker/texas_holdem/display_card_test.py
@@ -8,21 +8,42 @@ def test_face_card_heart():
 
 
 def test_face_card_club():
-    expected = '2♣
+    expected = '2♣'
     card = display_card('C2')
     assert expected == card
-#
-# def test_face_card_spade():
-#     expected = '2♠'
-#     card = display_card('S2')
-#     assert expected == card
-#
-# def test_face_card_diamonds():
-#     expected = '2♦'
-#     card = display_card('D2')
-#     assert expected == card
-#
-# def test_jack_of_diamonds():
-#     expected = 'J♦'
-#     card = display_card('D11')
-#     assert expected == card
+
+
+def test_face_card_spade():
+    expected = '2♠'
+    card = display_card('S2')
+    assert expected == card
+
+
+def test_face_card_diamonds():
+    expected = '2♦'
+    card = display_card('D2')
+    assert expected == card
+
+
+def test_jack_of_diamonds():
+    expected = 'J♦'
+    card = display_card('D11')
+    assert expected == card
+
+
+def test_queen_of_diamonds():
+    expected = 'Q♦'
+    card = display_card('D12')
+    assert expected == card
+
+
+def test_king_of_diamonds():
+    expected = 'K♦'
+    card = display_card('D13')
+    assert expected == card
+
+
+def test_ace_of_diamonds():
+    expected = 'A♦'
+    card = display_card('D14')
+    assert expected == card
