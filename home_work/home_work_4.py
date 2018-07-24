d05402c244f7afb3cafb628fcc2177ab80280c55
jrj92280 <jrj92280@gmail.com>
2018-07-23 21:55:20 -0400

    Summation

--- home_work/home_work_4.py
+++ home_work/home_work_4.py
@@ -1,22 +1,33 @@
-
-
+import time
 
 def nation_total(y):
-    t = 0
-    y = t*(t + 1)/2
-    t += y
-    return nation_total
+    start = time.time()
+    x = y * (y + 1) / 2
+    stop = time.time()
+    print(stop - start)
+    return x
 
 
-if x in t(y):
-    print(nation_total())
+print(nation_total(1000))
 
 
+def nation_total_loop(y):
+    start = time.time()
+    x = 0
+    for count in range(1, y + 1):
+        x += count
+    stop = time.time()
+    print(stop - start)
+    return x
 
 
-print (nation_total(8))
+print(nation_total_loop(1000))
 
 
+def nation_total_recursive(y):
+    if y == 1:
+        return y
+    return y + nation_total_recursive(y - 1)
 
 
-
+print(nation_total_recursive(100))
