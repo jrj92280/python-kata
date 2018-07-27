c463ec50f6abee83a0d7cf12f703d7316f408f0a
jrj92280 <jrj92280@gmail.com>
2018-07-26 22:59:26 -0400

    Print numbers practice

--- home_work/home_work_5.py
+++ home_work/home_work_5.py
@@ -1,21 +1,46 @@
 """
+https://adriann.github.io/programming_problems.html
+
 Elementary
 Write a program that prints ‘Hello World’ to the screen.
 Write a program that asks the user for their name and greets them with their name.
 Modify the previous program such that only the users Alice and Bob are greeted with their names.
+
 Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
 Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
-Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
-Write a program that prints a multiplication table for numbers up to 12.
-Write a program that prints all prime numbers. (Note: if your programming language does not support arbitrary size numbers, printing all primes up to the largest number you can easily represent is fine too.)
-Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. I counts only as one try if they input the same number multiple times consecutively.
-Write a program that prints the next 20 leap years.
 """
 
 
 def user():
-    x = input('Enter your name:')
-    return "Hello " + x
+    x = input('Enter your name: ')
+    greeting = 'Hello'
+
+    if x.lower() in ['alice', 'bob']:
+        greeting += ' ' + x
+
+    return greeting
 
 
-print(user())
+# print(user())print ('How are you doing?')
+
+
+def _print_numbers():
+    x = input('Enter a number: ')
+    x = int(x)
+    return x * (x + 1) / 2
+
+
+def print_numbers():
+    x = input('Enter a number: ')
+    x = int(x)
+
+    total = 0
+
+    for number in range(1, x + 1):
+        if number % 3 == 0 or number % 5 == 0:
+            total = total + number
+
+    return total
+
+
+print(print_numbers())
