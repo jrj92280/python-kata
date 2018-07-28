d9372d51ec40ba71f442aae82bec32cb9e6a2301
jrj92280 <jrj92280@gmail.com>
2018-07-28 14:39:33 -0400

    Dict containing a dict

--- chapter_3/vowels3.py
+++ chapter_3/vowels3.py
@@ -1,1 +1,21 @@
+people = {}
+people['Ford'] = { 'Name': 'Ford Prefect',
+                    'Gender': 'Male',
+                    'Occupation': 'Researcher',
+                    'Home Planet': 'Betelgeuse Seven'}
+print(people)
 
+people ['Arthur'] = { 'Name': 'Arthur Dent',
+                      'Gender': 'Male',
+                       'Occupation': 'Sandwich-Maker',
+                       'Home Planet': 'Earth' }
+
+people ['Trillian'] = {'Name': 'Tricia McMillan',
+                       'Bender': 'Female',
+                       'Occupation': 'Mathemtician',
+                        'Home Planet': 'Earth'}
+people['Robot'] = { 'Name': 'Marvin',
+                    'Gender': 'Unknown',
+                    'Occupation': 'Paranoid Android',
+                     'Home Planet': 'Unknown' }
+print(people)
