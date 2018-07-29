f88f08c75d142b4dc82ed6961662173e292b372d
jrj92280 <jrj92280@gmail.com>
2018-07-29 17:28:53 -0400

    Copy file

--- chapter_3/vowels3.py
+++ chapter_3/vowels3.py
@@ -1,23 +1,23 @@
 people = {}
-people['Ford'] = { 'Name': 'Ford Prefect',
+people['Ford'] = {'Name': 'Ford Prefect',
                     'Gender': 'Male',
                     'Occupation': 'Researcher',
                     'Home Planet': 'Betelgeuse Seven'}
 print(people)
 
-people ['Arthur'] = { 'Name': 'Arthur Dent',
+people['Arthur'] = {'Name': 'Arthur Dent',
                       'Gender': 'Male',
                        'Occupation': 'Sandwich-Maker',
-                       'Home Planet': 'Earth' }
+                       'Home Planet': 'Earth'}
 
-people ['Trillian'] = {'Name': 'Tricia McMillan',
+people['Trillian'] = {'Name': 'Tricia McMillan',
                        'Bender': 'Female',
                        'Occupation': 'Mathemtician',
                         'Home Planet': 'Earth'}
-people['Robot'] = { 'Name': 'Marvin',
+people['Robot'] = {'Name': 'Marvin',
                     'Gender': 'Unknown',
                     'Occupation': 'Paranoid Android',
-                     'Home Planet': 'Unknown' }
+                     'Home Planet': 'Unknown'}
 import pprint
 pprint.pprint(people)
 print(people['Arthur']['Occupation'])
