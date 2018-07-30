0abbf09256e47e7959ebc7cf04e2fe878620d9d8
jrj92280 <jrj92280@gmail.com>
2018-07-30 11:45:26 -0400

    Chapter 4 creating functions

---
+++ chapter_4/creating_function.py
@@ -0,0 +1,8 @@
+def search4vowels():
+    vowels = set('aeiou')
+    word = input("Provide a word to search for vowels: ")
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
+
+print(search4vowels())
