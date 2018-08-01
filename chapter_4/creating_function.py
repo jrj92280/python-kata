df325be7a958a3c04e64e6a608ef55480b868a43
jrj92280 <jrj92280@gmail.com>
2018-08-01 14:21:06 -0400

    Chapter 4 creating functions

--- chapter_4/creating_function.py
+++ chapter_4/creating_function.py
@@ -1,9 +1,8 @@
-def search4vowels():
+def search4vowels(word):
     """"Display any vowels found in an asked - for word."""
     vowels = set('aeiou')
-    word = input("Provide a word to search for vowels: ")
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
-    return found
-print(search4vowels())
+
+search4vowels('hitch-hiker')
