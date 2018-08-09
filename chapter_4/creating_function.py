e3cf591b6747ad5ad0034754f82cbbed9eb3045e
jrj92280 <jrj92280@gmail.com>
2018-08-08 23:14:38 -0400

    Chapter 4 creating functions

--- chapter_4/creating_function.py
+++ chapter_4/creating_function.py
@@ -1,39 +1,39 @@
-# def search4vowels(word):
-#     """"Return any vowels found in a supplied word."""
-#     vowels = set('aeiou')
-#     return vowels.intersection(set(word))
+def search4vowels(pharse :str) -> set:
+    """"Return any vowels found in a supplied word."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(pharse))
+
+
+print(search4vowels('hitch-hiker'))
+print(search4vowels('life, the universe and everything'))
+print(search4vowels('sky'))
+print(search4vowels('galaxy'))
+
+# '''list'''
 #
+# l = list()
+# print(l)
+# l = [1, 2, 3]
+# print(l)
 #
-# print(search4vowels('hitch-hiker'))
-# print(search4vowels('life, the universe and everything'))
-# print(search4vowels('sky'))
-# print(search4vowels('galaxy'))
-
-'''list'''
-
-l = list()
-print(l)
-l = [1, 2, 3]
-print(l)
-
-'''dictionary'''
-
-d = dict()
-print(d)
-d = {'first': 1, 'second': 2, 'third': 3}
-print(d)
-
-'''set'''
-
-s = set()
-print(s)
-s = {1, 2, 3}
-print(s)
-
-'''tuple'''
-
-t = tuple()
-print(t)
-t = (1, 2, 3)
-print(t)
-
+# '''dictionary'''
+#
+# d = dict()
+# print(d)
+# d = {'first': 1, 'second': 2, 'third': 3}
+# print(d)
+#
+# '''set'''
+#
+# s = set()
+# print(s)
+# s = {1, 2, 3}
+# print(s)
+#
+# '''tuple'''
+#
+# t = tuple()
+# print(t)
+# t = (1, 2, 3)
+# print(t)
+#
