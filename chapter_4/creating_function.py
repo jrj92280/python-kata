816015594c1ab190e53cc3103a9729c98e6cf3cd
jrj92280 <jrj92280@gmail.com>
2018-08-08 22:28:21 -0400

    Chapter 4 creating functions

--- chapter_4/creating_function.py
+++ chapter_4/creating_function.py
@@ -1,10 +1,10 @@
 def search4vowels(word):
     """"Return any vowels found in a supplied word."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    return found
+    return vowels.intersection(set(word))
+
 
 print(search4vowels('hitch-hiker'))
-
+print(search4vowels('life, the universe and everything'))
 print(search4vowels('sky'))
 print(search4vowels('galaxy'))
