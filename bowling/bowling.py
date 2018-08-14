883b3018bf62b84199ea31b1d3ffe28848ee11dc
jrj92280 <jrj92280@gmail.com>
2018-08-13 23:10:16 -0400

    Bowling kata

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,6 +1,8 @@
 class Bowling:
     def __init__(self, rolls):
         self.rolls = rolls
+        self.score = 0
 
     def calculate_score(self):
-        pass
+        if '1' in self.rolls:
+            self.score = 1
--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -2,6 +2,24 @@ from bowling.bowling import Bowling
 
 
 def test_zero():
+    # arrange
     rolls = '-- -- -- -- -- -- -- -- -- --'
     bowling = Bowling(rolls)
+
+    # act
     bowling.calculate_score()
+
+    # assert
+    assert 0 == bowling.score
+
+
+def test_one():
+    # arrange
+    rolls = '1- -- -- -- -- -- -- -- -- --'
+    bowling = Bowling(rolls)
+
+    # act
+    bowling.calculate_score()
+
+    # assert
+    assert 1 == bowling.score
