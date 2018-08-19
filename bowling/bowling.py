f19c0a72eb263a83238f558b22cd26cd36a3a853
jrj92280 <jrj92280@gmail.com>
2018-08-19 16:37:54 -0400

    Set up bowling kata

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -3,6 +3,8 @@ def score_game(turns: str) -> int:
     assert len(frames) == 10
 
     score = 0
+    is_spare = False
+
     for frame in frames:
         assert len(frame) == 2 or len(frame) == 1
 
@@ -12,14 +14,26 @@ def score_game(turns: str) -> int:
         turn_1 = frame[0]
         turn_2 = frame[1]
 
-        try:
-            score += int(turn_1)
-        except:
-            pass
+        if is_spare:
+            score = score_turn(score, turn_1)
+            is_spare = False
 
-        try:
-            score += int(turn_2)
-        except:
-            pass
+        if turn_2 == '/':
+            score += 10
+            is_spare = True
+            continue
+
+        score = score_turn(score, turn_1)
+        score = score_turn(score, turn_2)
+
+    return score
+
+
+def score_turn(score, turn):
+    try:
+        score += int(turn)
+    except:
+        if turn == 'X':
+            score += 10
 
     return score
--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -29,4 +29,29 @@ def test_score_n():
 
     assert score == 8
 
+
+def test_score_spare():
+    turns = frames()
+    turns = "1/" + turns[2:]
+    score = score_game(turns)
+
+    assert score == 10
+
+
+def test_score_spare_1():
+    turns = frames()
+    turns = "1/ 1" + turns[4:]
+    score = score_game(turns)
+
+    assert score == 12
+
+
+def test_score_spare_x():
+    turns = frames()
+    turns = "1/ X" + turns[4:]
+    score = score_game(turns)
+
+    assert score == 30
+
+
 print(("-- ".join(["" for _ in range(11)])).strip())
