f1c24e8a03edb18c37f2f641be77fbb4a60a4b28
jrj92280 <jrj92280@gmail.com>
2018-08-22 23:11:33 -0400

    Bowling kata

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,56 +1,103 @@
+min_frames = 10
+max_frames = 12
+
+vaild_turn_one_values = "-123456789X"
+vaild_turn_two_values = "-123456789/"
+
+turn_one_index = 0
+turn_two_index = 1
+
+
 def score_game(turns: str) -> int:
     frames = get_frames(turns)
+    validate_frames(frames)
 
     score = 0
-    is_spare = False
+    previous_was_spare = False
 
     for frame_index, frame in enumerate(frames):
-        assert len(frame) == 2 or len(frame) == 1
+        score += score_frame(frame_index, frame, previous_was_spare)
+        previous_was_spare = is_spare(frame)
 
-        if frame_index == 10 and len(frame) == 2:
-            assert len(frames) == 11
+    return score
 
-        if frame_index == 11:
-            assert len(frame) == 1
 
-        if len(frame) == 1:
-            continue
+def score_frame(frame_index: int, frame: str, previous_was_spare: bool) -> int:
+    score = 0
 
-        turn_1 = frame[0]
-        if turn_1 not in "-123456789X":
-            assert False
-        turn_2 = frame[1]
+    assert len(frame) == 2 or len(frame) == 1
 
-        if turn_2 not in "-123456789/":
-            assert False
+    if frame_index == 11:
+        assert len(frame) == 1
 
-        if is_spare:
-            score = score_turn(score, turn_1)
-            is_spare = False
+    if len(frame) == 1:
+        return 0
 
-        if turn_2 == '/':
-            score += 10
-            is_spare = True
-            continue
+    turn_1 = validate_turn(frame, turn_one_index, vaild_turn_one_values)
+    turn_2 = validate_turn(frame, turn_two_index, vaild_turn_two_values)
 
+    if previous_was_spare:
+        score = score_turn(score, turn_1)
+
+    if is_spare(turn_2):
+        score += 10
+    else:
         score = score_turn(score, turn_1)
         score = score_turn(score, turn_2)
 
     return score
 
-
 def get_frames(turns):
     frames = turns.split(' ')
-
-    if len(frames) < 10:
-        assert False
-    if len(frames) > 12:
-        assert False
-    if frames[9] == "X" or "/" in frames[9]:
-        pass
-    else:
-        assert len(frames) == 10
     return frames
+
+
+def validate_frames(frames):
+    frame_count = len(frames)
+
+    tenth_frame = get_tenth_frame(frames)
+    eleventh_frame = get_eleventh_frame(frames)
+
+    if frame_count < min_frames or frame_count > max_frames:
+        assert False
+
+    if not is_strike(tenth_frame) and not is_spare(tenth_frame):
+        assert frame_count == 10
+
+    if is_spare(eleventh_frame):
+        assert frame_count == 11
+
+
+
+def get_tenth_frame(frames):
+    return get_frame(frames, 9)
+
+
+def get_eleventh_frame(frames):
+    return get_frame(frames, 10)
+
+
+def get_frame(frames, index):
+    frame_count = len(frames)
+    return frames[index] if frame_count > index else None
+
+
+def is_spare(frame):
+    return frame and '/' in frame
+
+
+def is_strike(frame):
+    return 'X' in frame
+
+
+def has_twleve_frames(frames):
+    return len(frames) == 12
+
+def validate_turn(frame, turn_index, vaild_turn_one_values):
+    turn_1 = frame[turn_index]
+    if turn_1 not in vaild_turn_one_values:
+        assert False
+    return turn_1
 
 
 def score_turn(score, turn):
--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -3,18 +3,16 @@ import pytest
 from bowling.bowling import score_game
 
 
+import pytest
+
+from bowling.bowling import score_game
+
+
 def test_score_0():
     turns = frames()
 
     score = score_game(turns)
     assert score == 0
-
-
-def frames():
-    ten_frames = ["--" for _ in range(10)]
-    turns = " ".join(ten_frames)
-    return turns
-
 
 def test_score_1():
     turns = frames()
@@ -88,4 +86,7 @@ def test_score_boundary_frames():
     for good_scenario in good_scenarios:
         score_game(good_scenario)
 
-print(("-- ".join(["" for _ in range(11)])).strip())
+def frames():
+    ten_frames = ["--" for _ in range(10)]
+    turns = " ".join(ten_frames)
+    return turns
