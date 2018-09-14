0160c5f747972d60d3206c9620bf171a91eef3b1
jrj92280 <jrj92280@gmail.com>
2018-09-14 11:01:45 -0400

    Betting for Texas Holdem started

--- poker/betting/__init__.py
+++ poker/betting/__init__.py
@@ -1,5 +1,4 @@
 def betting(bet,chips,player,check,bet_or_raise,call,fold,button,ante):
-    bet == bet
-    len([5000]) == bet
-    return len([200, 5000])
+    chips == chips + int[5000]
+    return
 
