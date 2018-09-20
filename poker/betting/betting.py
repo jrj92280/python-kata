e1f1d042610b64055db43c35b9af8c799328a594
jrj92280 <jrj92280@gmail.com>
2018-09-20 01:13:39 -0400

    What I think I need to create

--- poker/betting/betting.py
+++ poker/betting/betting.py
@@ -1,3 +1,5 @@
-def betting(bet, chips, player, check, bet_or_raise, call, fold, button, ante):
-    _chips = chips + 6000
-    return
+def
+
+
+
+#bet, chips, player, check, bet_or_raise, call, fold, button, ante
