44b32e0e95ebe0a9e8e8706c63c7d448e9a86636
jrj92280 <jrj92280@gmail.com>
2018-10-23 19:17:10 -0400

    Creating a pass code kata

---
+++ head_first/chapter_2.py
@@ -0,0 +1,25 @@
+# cup1 = 0
+# cup2 = 1
+# cup3 = 0
+# cup1 = cup1 + 1
+# cup2 = cup1 - 1
+# cup3 =cup1
+# cup1= cup1 * 0
+# cup2 = cup3
+# cup3 = cup1
+# cup1 = cup2 % 1
+# cup3 = cup2
+# cup2 = cup3 - cup3
+
+
+word1 = 'ox'
+word2 = 'owl'
+word3 = 'cow'
+word4 = 'sheep'
+word5 = 'files'
+word6 = 'trots'
+word7 = 'runs'
+word8 = 'blue'
+word9 = 'red'
+word10 = 'yellow'
+word9 = ' The' + word9
