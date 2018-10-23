b08557f15a0841acbd5bebb3126cdb9cbdd7aa66
jrj92280 <jrj92280@gmail.com>
2018-10-23 19:23:28 -0400

    create a pass word kata

--- head_first/chapter_2.py
+++ head_first/chapter_2.py
@@ -22,4 +22,11 @@ word7 = 'runs'
 word8 = 'blue'
 word9 = 'red'
 word10 = 'yellow'
-word9 = ' The' + word9
+word9 = ' The ' + word9
+passcode = word8
+passcode = word9
+passcode = passcode + ' f'
+passcode = passcode + word1
+passcode = passcode + " "
+passcode = passcode + word6
+print(passcode)
