476db2a82daaac0a1751b906bb9f3852f2182409
jrj92280 <jrj92280@gmail.com>
2018-11-19 16:35:36 -0500

    Swap variable values

--- head_first/chapter_2.py
+++ head_first/chapter_2.py
@@ -11,22 +11,31 @@
 # cup3 = cup2
 # cup2 = cup3 - cup3
 
+#
+# word1 = 'ox'
+# word2 = 'owl'
+# word3 = 'cow'
+# word4 = 'sheep'
+# word5 = 'files'
+# word6 = 'trots'
+# word7 = 'runs'
+# word8 = 'blue'
+# word9 = 'red'
+# word10 = 'yellow'
+# word9 = ' The ' + word9
+# passcode = word8
+# passcode = word9
+# passcode = passcode + ' f'
+# passcode = passcode + word1
+# passcode = passcode + " "
+# passcode = passcode + word6
+# print(passcode)
 
-word1 = 'ox'
-word2 = 'owl'
-word3 = 'cow'
-word4 = 'sheep'
-word5 = 'files'
-word6 = 'trots'
-word7 = 'runs'
-word8 = 'blue'
-word9 = 'red'
-word10 = 'yellow'
-word9 = ' The ' + word9
-passcode = word8
-passcode = word9
-passcode = passcode + ' f'
-passcode = passcode + word1
-passcode = passcode + " "
-passcode = passcode + word6
-print(passcode)
+first = ('somewhere')
+last = ('over the rainbow')
+print(first, last)
+
+temp = first
+first = last
+last = temp
+print(first, last)
