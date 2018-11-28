683346b4232be6222519879266c46ab44bb2dee8
jrj92280 <jrj92280@gmail.com>
2018-11-28 00:10:53 -0500

    Finished paper rock scissors

--- head_first/paper_rocks_scissors.py
+++ head_first/paper_rocks_scissors.py
@@ -1,16 +1,30 @@
 import random
 
-random_choice = random.randint(0, 2)
-if random_choice == 0:
-    random_choice = 'Rocks'
-elif random_choice == 1:
-    random_choice = 'Paper'
+computer_choice = random.randint(0, 2)
+if computer_choice == 0:
+    computer_choice = 'rock'
+elif computer_choice == 1:
+    computer_choice = 'paper'
 else:
-    random_choice = 'Scissors'
+    computer_choice = 'scissors'
 
 user_choice = input('rock, paper or scissors? ')
-print('You chose', user_choice, 'and the computer chose', random_choice)
+# print('You chose', user_choice, 'and the computer chose', computer_choice)
 
-choices = ['rock', 'paper', 'scissors']
-computer_choice = random.choice(choices)
-print(computer_choice)
+# choices = ['rock', 'paper', 'scissors']
+# computer_choice = random.choice(choices)
+# print(computer_choice)
+
+winner = ('tie', 'user', 'computer')
+if computer_choice == user_choice:
+    winner = 'tie'
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    winner = 'computer'
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'computer'
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = 'computer'
+else:
+    winner = 'user'
+
+print('The winner is', winner)
