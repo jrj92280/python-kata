e3c88b490c62962bc5bc45a21a85891aa8e0db2b
jrj92280 <jrj92280@gmail.com>
2018-12-15 14:43:23 -0500

    Runing list forwards then backwards

--- learn_to_code_chapter_4/page_130_list.py
+++ learn_to_code_chapter_4/page_130_list.py
@@ -1,25 +1,45 @@
-eighties = ['', 'duran duran', 'B-52s', 'muse']
-newwave = ['flock of seagulls', 'postal service']
+# eighties = ['', 'duran duran', 'B-52s', 'muse']
+# newwave = ['flock of seagulls', 'postal service']
+#
+# remember = eighties[1]
+#
+# eighties[1] = 'culture club'
+#
+# band = newwave[0]
+#
+# eighties[3] = band
+#
+# eighties[0] = eighties[2]
+#
+# eighties[2] = remember
+#
+# print(eighties)
+#
+# length = len(eighties)
+# print(length)
+# last = eighties[length - 1]
+# print(last)
+# second_last = eighties[length - 2]
+# print(second_last)
+# third_last = eighties[length - 3]
+# print((third_last))
 
-remember = eighties[1]
 
-eighties[1] = 'culture club'
+# ///////////The Thing-A-Ma-Jig//////////////// page 133
 
-band = newwave[0]
+characters = ['t', 'a', 'c', 'o']
+output = ''
+length = len(characters)
+i = 0
+while (i < length):
+    output = output + characters[i]
+    i = i + 1
 
-eighties[3] = band
+length = length * -1
+i = -2
 
-eighties[0] = eighties[2]
+while (i >= length):
+    output = output + characters[i]
+    i = i - 1
 
-eighties[2] = remember
-
-print(eighties)
-
-length = len(eighties)
-print(length)
-last = eighties[length - 1]
-print(last)
-second_last = eighties[length - 2]
-print(second_last)
-third_last = eighties[length - 3]
-print((third_last))
+print(output)
