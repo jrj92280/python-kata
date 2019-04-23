371171f5861bacbca4df56252e94f25ed3f24b61
jrj92280 <jrj92280@gmail.com>
2019-04-22 23:09:20 -0400

    page 189

--- head_first_learn_to_code/chapter_5.py
+++ head_first_learn_to_code/chapter_5.py
@@ -1,3 +1,5 @@
+print("Get those dogs ready")
+
 def bark(name, weight):
     if weight > 20:
         print(name, "says WOOF WOOF")
@@ -6,4 +8,14 @@ def bark(name, weight):
 
 
 bark('Cody', 40)
+bark('Sparky', 9)
+bark('Jackson', 12)
+bark('Fido', 65)
+bark('Speedy', 20)
+bark('Barnaby', -1)
+bark('Scottie', 0)
+bark('Lady', 20)
+bark('Spot', 10)
+bark('Rover', 21)
+
 print("Okay, we're all done")
