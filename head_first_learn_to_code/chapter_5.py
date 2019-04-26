051212f5f6a6b6756100cb126b15bd5cd00f09bc
jrj92280 <jrj92280@gmail.com>
2019-04-25 22:53:58 -0400

    Page l91 how should I get there

--- head_first_learn_to_code/chapter_5.py
+++ head_first_learn_to_code/chapter_5.py
@@ -1,21 +1,35 @@
-print("Get those dogs ready")
+# print("Get those dogs ready")
+#
+# def bark(name, weight):
+#     if weight > 20:
+#         print(name, "says WOOF WOOF")
+#     else:
+#         print(name, 'says woof woof')
+#
+#
+# bark('Cody', 40)
+# bark('Sparky', 9)
+# bark('Jackson', 12)
+# bark('Fido', 65)
+# bark('Speedy', 20)
+# bark('Barnaby', -1)
+# bark('Scottie', 0)
+# bark('Lady', 20)
+# bark('Spot', 10)
+# bark('Rover', 21)
+#
+# print("Okay, we're all done")
 
-def bark(name, weight):
-    if weight > 20:
-        print(name, "says WOOF WOOF")
+# ///page 191///
+def how_should_I_get_there(miles):
+    if miles > 120:
+        print('Take a plane')
+    elif miles >= (2):
+        print('Take a car')
     else:
-        print(name, 'says woof woof')
+        print('Walk')
 
 
-bark('Cody', 40)
-bark('Sparky', 9)
-bark('Jackson', 12)
-bark('Fido', 65)
-bark('Speedy', 20)
-bark('Barnaby', -1)
-bark('Scottie', 0)
-bark('Lady', 20)
-bark('Spot', 10)
-bark('Rover', 21)
-
-print("Okay, we're all done")
+how_should_I_get_there(800.3)
+how_should_I_get_there(2)
+how_should_I_get_there(.5)
