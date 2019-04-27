967146d304638cfdde850dd402546ac866bf0440
jrj92280 <jrj92280@gmail.com>
2019-04-26 23:36:38 -0400

    Get attribute function page 199

--- head_first_learn_to_code/avatar.py
+++ head_first_learn_to_code/avatar.py
@@ -1,29 +1,47 @@
-hair = input("What color hair[brown]?")
-if hair == '':
-    hair = 'brown'
-print('You chose', hair)
+# hair = input("What color hair[brown]?")
+# if hair == '':
+#     hair = 'brown'
+# print('You chose', hair)
+#
+# hair_length = input("What hair length [short]?")
+# if hair_length == '':
+#     hair_length = 'short'
+# print('You chose', hair_length)
+#
+# eyes = input("What eye color [blue]?")
+# if eyes == '':
+#     eyes = 'blue'
+# print("You chose', eyes")
+#
+# gender = input("What gender [female]?")
+# if gender == '':
+#     gender = 'female'
+# print('You chose gender', gender)
+#
+# has_glasses = input("Has glasses [no]?")
+# if has_glasses == '':
+#     has_glasses = 'no'
+# print('You chose', has_glasses)
+#
+# has_beard = input("Has beard [no]?")
+# if has_beard == '':
+#     has_beard = 'no'
+# print('You chose', has_beard)
 
-hair_length = input("What hair length [short]?")
-if hair_length == '':
-    hair_length = 'short'
-print('You chose', hair_length)
+# /// page 199//
 
-eyes = input("What eye color [blue]?")
-if eyes == '':
-    eyes = 'blue'
-print("You chose', eyes")
+def get_attribute(query, default):
+    question = query + ' [' + default + ']? '
+    answer = input(question)
+    if (answer == ''):
+        answer = default
+    print('You chose)', answer)
+    return answer
 
-gender = input("What gender [female]?")
-if gender == '':
-    gender = 'female'
-print('You chose gender', gender)
 
-has_glasses = input("Has glasses [no]?")
-if has_glasses == '':
-    has_glasses = 'no'
-print('You chose', has_glasses)
-
-has_beard = input("Has beard [no]?")
-if has_beard == '':
-    has_beard = 'no'
-print('You chose', has_beard)
+hair = get_attribute('What hair color', 'brown')
+hair_length = get_attribute('What hair length', 'short')
+eye = get_attribute('What eye color', 'blue')
+gender = get_attribute('What gender', 'female')
+glasses = get_attribute('Sas glasses', 'no')
+beard = get_attribute('Has beard', 'no')
