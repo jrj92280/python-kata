8cfc4993df7e9089f18655013710d7b382eb76d7
jrj92280 <jrj92280@gmail.com>
2019-04-28 23:09:00 -0400

    Page 207

--- head_first_learn_to_code/chapter_5.py
+++ head_first_learn_to_code/chapter_5.py
@@ -54,7 +54,9 @@ greeting = 'Greeetings'
 
 def greet(name, message):
     global greeting
+    greeting = 'Hi'
     print(greeting, name + '.', message)
 
 
 greet('June', 'See you soon!')
+print(greeting)
