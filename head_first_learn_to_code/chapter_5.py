d464add9ae383281aac1ba73185ba147ff4ee46a
jrj92280 <jrj92280@gmail.com>
2019-05-03 18:45:11 -0400

    Page 215 needs work

--- head_first_learn_to_code/chapter_5.py
+++ head_first_learn_to_code/chapter_5.py
@@ -63,28 +63,47 @@
 
 # ///page 210///
 
-def make_sundae(ice_cream='vanilla', sauce='chocolate', nuts=True, banana=True, brownies=False, whipped_cream=True):
-    recipe = ice_cream + 'ice cream and ' + sauce + 'sauce'
-    if nuts:
-        recipe = recipe + 'with nuts and '
-    if banana:
-        recipe = recipe + 'a banana and'
-    if brownies:
-        recipe = recipe + 'a brownie and '
-    if not whipped_cream:
-        recipe = recipe + 'no '
-    recipe = recipe + 'whipped cream on top.'
-    return recipe
+# def make_sundae(ice_cream='vanilla', sauce='chocolate', nuts=True, banana=True, brownies=False, whipped_cream=True):
+#     recipe = ice_cream + 'ice cream and ' + sauce + 'sauce'
+#     if nuts:
+#         recipe = recipe + 'with nuts and '
+#     if banana:
+#         recipe = recipe + 'a banana and'
+#     if brownies:
+#         recipe = recipe + 'a brownie and '
+#     if not whipped_cream:
+#         recipe = recipe + 'no '
+#     recipe = recipe + 'whipped cream on top.'
+#     return recipe
+#
+#
+# sundae = make_sundae()
+# print('One sundae coming up with', sundae)
+#
+# sundae = make_sundae('chocolate')
+# print('One sundae coming up with', sundae)
+#
+# sundae = make_sundae(sauce='caramel', whipped_cream=False, banana=False)
+# print('One sundae coming up with', sundae)
+#
+# sundae = make_sundae(whipped_cream=False, banana=True, brownies=True, ice_cream='peanut butter')
+# print('One sundae coming up with', sundae)
+
+# // page 215///
+
+balance = 10500
+camera_on = True
 
 
-sundae = make_sundae()
-print('One sundae coming up with', sundae)
+def steal(blance, amount):
+    global camera_on
+    camera_on = False
 
-sundae = make_sundae('chocolate')
-print('One sundae coming up with', sundae)
+    if (amount < balance):
+        balance = balance - amount
+        return amount
+        camera_on = True
 
-sundae = make_sundae(sauce='caramel', whipped_cream=False, banana=False)
-print('One sundae coming up with', sundae)
 
-sundae = make_sundae(whipped_cream=False, banana=True, brownies=True, ice_cream='peanut butter')
-print('One sundae coming up with', sundae)
+proceeds = steal(balance, 1250)
+print('Criminal:  you stole', proceeds)
