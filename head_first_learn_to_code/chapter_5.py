3310aad6c37eb392e392608606ad6f06fd16a1ce
jrj92280 <jrj92280@gmail.com>
2019-05-24 17:37:18 -0400

    Fixed spelling errors

--- head_first_learn_to_code/chapter_5.py
+++ head_first_learn_to_code/chapter_5.py
@@ -90,20 +90,20 @@
 # print('One sundae coming up with', sundae)
 
 # // page 215///
-
-balance = 10500
-camera_on = True
-
-
-def steal(balance, amount):
-    global camera_on
-    camera_on = False
-
-    if (amount < balance):
-        balance = balance - amount
-        return amount
-        camera_on = True
-
-
-proceeds = steal(balance, 1250)
-print('Criminal:  you stole', proceeds)
+#
+# balance = 10500
+# camera_on = True
+#
+#
+# def steal(balance, amount):
+#     global camera_on
+#     camera_on = False
+#
+#     if (amount < balance):
+#         balance = balance - amount
+#         return amount
+#         camera_on = True
+#
+#
+# proceeds = steal(balance, 1250)
+# print('Criminal:  you stole', proceeds)
